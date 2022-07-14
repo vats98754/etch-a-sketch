624899c18222331178c4be898ed33d41a50ff687
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-15 06:23:22 +0800

    Custom grid size working; Modify styling; Grid size from 1-100 per side

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+const customBtn = document.querySelector('#btn-grid-size');
+// Draws new custom grid when button is clicked
+customBtn.addEventListener('click', function() {
+    const row = prompt('Enter number of rows', '1');
+    const column = prompt('Enter number of columns', '1');
+    let rowNum = parseInt(row);
+    let colNum = parseInt(column);
+    rowNum = Math.ceil(1, Math.floor(200, rowNum));
+    colNum = Math.ceil(1, Math.floor(200, colNum));
+    drawGrid(row, column);
+});
+
 const container = document.querySelector('#container');
 
-const rows = 16;
-const columns = 16;
-
-for (let i = 0; i < rows; ++i) {
-    const divParent = document.createElement('div');
-    divParent.classList.add('row');
-    for (let j = 0; j < columns; ++j) {
-        const div = document.createElement('div');
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'black';
-        });
-        div.classList.add('cell');
-        divParent.appendChild(div);
+// Function that creates grid within container given row and column numbers
+function drawGrid(rows, columns){
+    container.innerHTML = '';
+    for (let i = 0; i < columns; ++i) {
+        const divParent = document.createElement('div');
+        divParent.classList.add('row');
+        for (let j = 0; j < rows; ++j) {
+            const div = document.createElement('div');
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'black';
+            });
+            div.classList.add('cell');
+            divParent.appendChild(div);
+        }
+        container.appendChild(divParent);
     }
-    container.appendChild(divParent);
 }
+drawGrid(16, 16); // defaults to 16x16 grid
